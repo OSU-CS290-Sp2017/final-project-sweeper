2811b8830c219f1df469c0c0a47ab61fd922d5bd
Henry Peterson <peterhen@oregonstate.edu>
2017-06-05 15:57:31 -0700

    Added semicolons on every fucking line in server.js. also things work now

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-*Sweeper
- henry is a huuuuge bitch
-
-This is our sweeper.
+# *sweeper
+This is the final project for cs290 sp2017.
+Written by Henry Peterson, Martin Barker, Louis Duvoisin, and bailey singleton.
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-var express = require('express')
-var expressHandles = require('express-handlebars')
-var app = express()
-var boardData = require('./boardData.json') //requires board file, game state
+var express = require('express');
+var fs = require('fs');
+var expressHandles = require('express-handlebars');
+var app = express();
+var boardData = require('./map.json'); //requires board file, game state
 
+var cssContent = fs.readFileSync('./public/style.css', 'utf-8');
 
 app.engine('handlebars', expressHandles({defaultLayout:'main'})); 
+app.set('view engine', 'handlebars');
 //routing ('', {params})
 
 app.get('/', function(req, res){
+    res.status(200);
+	res.render('minePage', {row: boardData.board, modal: true});
+});
 
-	res.render('boardData', {row: boardData.})
-})
+app.get('/style.css', function(req, res){
+    res.status(200);
+    res.end(cssContent);
+});
 
 app.post('/',function(req, res){
-	res.send('hey guys')
-})
+	res.send('hey guys');
+});
 
-app.listen(3000)
+app.listen(3000);
