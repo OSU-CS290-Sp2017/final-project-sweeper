e1d8e15b02633c2a7eeb6995c61eb6f8b9cd3b04
lduv1 <duvoisil@oregonstate.edu>
2017-06-05 10:47:26 -0700

    added outputing map, testing.json contains outputted map file. It does not look pretty.

--- sweeper.js
+++ sweeper.js
@@ -1,10 +1,12 @@
+var fs = require('fs');
 
 var gameState = {
-  "rows": 10,
-  "cols": 10,
-  "totalMines": 8,
-  "remainingMines": 8,
-  "board": []
+    "dead": false,
+    "rows": 10,
+    "cols": 10,
+    "totalMines": 0,
+    "remainingMines": 0,
+    "board": []
 }
 playGame();
 
@@ -40,6 +42,28 @@ function printDebugMap(){
         }
         console.log(currRow);
     }
+}
+
+function printMap(){
+
+    for (var i = 0; i < gameState.cols; i++) {
+        var currRow = [];
+        for (var j = 0; j < gameState.rows; j++) {
+            if(gameState.board[i][j].flagged){
+                currRow[j] = 'F';
+            } else if(gameState.board[i][j].cleared){
+                currRow[j] = gameState.board[i][j].value;
+            } else {
+                currRow[j] = '*';
+            }
+        }
+        console.log(currRow);
+    }
+}
+
+function saveMap(){
+    var out = JSON.stringify(gameState);
+    fs.writeFileSync("testing.json", out);
 
 }
 
@@ -103,8 +127,87 @@ function countNearby(i,j){
     gameState.board[i][j].value = counter;
 }
 
+function takeTurn(){
+    markMap(2,4,1);
+    printMap();
+    console.log("marked 1");
+    markMap(7,8,2);
+    printMap();
+    console.log("marked 2");
+    markMap(9,9,1);
+    printMap();
+    console.log("marked 3");
+}
+
+function markMap(x,y,type){
+    if(type == 1){       //left click
+        if(gameState.board[x][y].mine == true){
+            gameState.dead = true;
+            displayLocation(x,y);
+        }
+        else if(gameState.board[x][y].value == 0){
+            displayLocation(x,y);
+            clearNearby(x,y);
+        } else{
+            displayLocation(x,y);
+        }
+    } else if(type == 2){   //right click
+        if(gameState.board[x][y].flagged == false){
+            gameState.board[x][y].flagged = true;
+        } else{
+            gameState.board[x][y].flagged = false;
+        }
+    } else if(type == 3){    //recursive clear nearby
+        if(gameState.board[x][y].value == 0 && gameState.board[x][y].cleared == false){
+            displayLocation(x,y);
+            clearNearby(x,y);
+        } else{
+            displayLocation(x,y);
+        }
+    }
+
+
+}
+function clearNearby(i,j){
+
+    if (i-1 >= 0 && j-1 >= 0 )
+        markMap(i-1,j-1,3);
+
+    if (i-1 >= 0)
+        markMap(i-1,j,3);
+
+    if (i-1 >= 0 && j+1 < gameState.cols )
+        markMap(i-1,j+1,3);
+
+    if (j-1 >= 0 )
+        markMap(i,j-1,3);
+
+    if (j+1 < gameState.cols )
+        markMap(i,j+1,3);
+
+    if (i+1 < gameState.rows && j-1 >= 0 )
+        markMap(i+1,j-1,3);
+
+    if (i+1 < gameState.rows)
+        markMap(i+1,j,3);
+
+    if (i+1 < gameState.rows && j+1 < gameState.cols )
+        markMap(i+1,j+1,3);
+
+}
+
+//displays the value of the current location, needed so we can actually
+//change the page in the future.
+function displayLocation(x,y){
+    gameState.board[x][y].cleared = true;
+}
+
 function playGame(){
     initializeMap();
     printDebugMap();
+    //while(gameState.dead == false){
+        takeTurn();
+    //}
+    saveMap();
 
 }
