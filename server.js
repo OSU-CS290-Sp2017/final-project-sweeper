8f64103fe425f723942078e9e5f785b96a875b47
Henry Peterson <peterhen@oregonstate.edu>
2017-06-07 11:28:37 -0700

    updated server

--- server.js
+++ server.js
@@ -24,14 +24,21 @@ app.get('/style.css', function(req, res){
     res.end(cssContent);
 });
 
+/*
 app.get('/index.js', function(req, res){
     res.status(200);
     res.end(indexJsContent);
 });
+*/
 
 app.get('/mineTemplate.js', function(req, res){
     res.status(200);
     res.end(mineTemplateContent);
+});
+
+app.get('*', function(req, res){
+    res.status(404);
+    res.render('404page');
 });
 
 app.post('/',function(req, res){
